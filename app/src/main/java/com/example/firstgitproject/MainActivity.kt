07b2c454b3233fca895ef752edd12c40bab2eb4a
mobin <mobin1384ahmadi1384@gmail.com>
2023-07-26 22:11:25 +0330

    add toast

--- app/src/main/java/com/example/firstgitproject/MainActivity.kt
+++ app/src/main/java/com/example/firstgitproject/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        android.widget.Toast.makeText(this, "hellow every body", Toast.LENGTH_SHORT).show()
     }
 }
