a17fc2a85adcb39bf3911532a9affa3d81503f14
mobin <mobin1384ahmadi1384@gmail.com>
2023-07-28 18:10:31 +0330

    add 2 item toast in app

--- app/src/main/java/com/example/firstgitproject/MainActivity.kt
+++ app/src/main/java/com/example/firstgitproject/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         android.widget.Toast.makeText(this, "hellow every body", Toast.LENGTH_SHORT).show()
+        android.widget.Toast.makeText(this, "hellow every body", Toast.LENGTH_SHORT).show()
+        android.widget.Toast.makeText(this, "hellow every body", Toast.LENGTH_SHORT).show()
     }
 }
