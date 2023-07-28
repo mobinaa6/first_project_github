cebbfbec157ef06b43a7d65d3b903acd76861968
mobin <mobin1384ahmadi1384@gmail.com>
2023-07-28 18:24:32 +0330

    i am in develop

--- app/src/main/java/com/example/firstgitproject/MainActivity.kt
+++ app/src/main/java/com/example/firstgitproject/MainActivity.kt
@@ -12,5 +12,6 @@ class MainActivity : AppCompatActivity() {
         android.widget.Toast.makeText(this, "hellow every body", Toast.LENGTH_SHORT).show()
         android.widget.Toast.makeText(this, "hellow every body", Toast.LENGTH_SHORT).show()
         android.widget.Toast.makeText(this, "hellow every body develop", Toast.LENGTH_SHORT).show()
+        android.widget.Toast.makeText(this, "i am in develop", Toast.LENGTH_SHORT).show()
     }
 }
